fb00cf0a4693c62ce6f578cfade0fb57e1e8d684
Marvin Santiago <marvin.santiago@ymail.com>
2015-07-14 17:16:03 -0500

    Completed Part 3, assignment is completely done

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -98,14 +98,20 @@ console.assert(GCD(50,20) === 10);
 //  * write a function that prints out the Least Common Multiple of two numbers
 //  */
 
-// function LCM(a, b){
-//     // YOUR CODE HERE
-// }
+function LCM(a, b){
+    // YOUR CODE HERE
+    if(a === 0 || b ===0){
+        return 1;
+    }
+    else{
+        return a * b / GCD(a, b);
+    }
+}
 
-// console.assert(LCM(10,10) === 10)
-// console.assert(LCM(2,5) === 10)
-// console.assert(LCM(3,6) === 6)
-// console.assert(LCM(0,1) === 1)
+console.assert(LCM(10,10) === 10);
+console.assert(LCM(2,5) === 10);
+console.assert(LCM(3,6) === 6);
+console.assert(LCM(0,1) === 1);
 
 // /**
 //  * Part 4
